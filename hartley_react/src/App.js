ad367ffd4fe1c2747504439a70bb0d3c6835ba8f
Daniel Hartley <danielhartley117@gmail.com>
2022-03-07 11:30:36 -0500

    broken live reload

--- hartley_react/src/App.js
+++ hartley_react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.vfsljkhdflkasjhdflkasjdhflkjh
+          <code>src/App.js</code> and to reload. This should be in jfjfjfjfj  
         </p>
         <a
           className="App-link"
