e8ecbba832a37c870f95fccfcc269dd20ecb7477
Daniel Hartley <danielhartley117@gmail.com>
2022-03-08 12:41:58 -0500

    update gitignore, added nodemon, not working

--- hartley_react/src/App.js
+++ hartley_react/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>src/App.js</code> and to reload. This should be in jfjfjfjfj  
+          Hello Ethan some
+          <code>src/App.js</code> and to reload.
         </p>
         <a
           className="App-link"
