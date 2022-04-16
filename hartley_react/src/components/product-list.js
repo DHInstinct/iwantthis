dd0afb8e301b4dc580f2aad7f21fa1727ac5e371
Daniel Hartley <danielhartley117@gmail.com>
2022-04-15 22:39:27 -0400

    get products and single prod php, filtering of products, and organization of etc components

--- hartley_react/src/components/product-list.js
+++ hartley_react/src/components/product-list.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import SingleProd from "../components/index";
-
-
+import SingleProd from "./etc/DisplaySingleProd";
 
 export default function ProductList() {
 
     const [products, setProducts] = useState([]);
+
 
     function HandleProducts() {
         axios.get("http://localhost:80/getproducts.php")
@@ -16,7 +15,6 @@ export default function ProductList() {
             .catch(function (error) {
                 console.log("Error getting products", error)
             })
-
     }
 
 
@@ -25,6 +23,77 @@ export default function ProductList() {
             return <SingleProd obj={object} key={i} />
         })
 
+    }
+
+    function clearFilters() {
+        let elementCount = document.getElementsByClassName('category1');
+
+        for (var i = 0; i < elementCount.length; i++) {
+            document.getElementsByClassName('category1')[i].style.visibility = 'visible';
+            document.getElementsByClassName('category2')[i].style.visibility = 'visible';
+            document.getElementsByClassName('category3')[i].style.visibility = 'visible';
+        }
+    }
+
+    let toggle1 = true;
+    function FilteriPhones() {
+
+        let elementCount = document.getElementsByClassName('category1');
+        if (toggle1) {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category2')[i].style.visibility = 'hidden';
+                document.getElementsByClassName('category3')[i].style.visibility = 'hidden';
+            }
+        }
+        else {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category2')[i].style.visibility = 'visible';
+                document.getElementsByClassName('category3')[i].style.visibility = 'visible';
+            }
+        }
+
+        toggle1 = !toggle1
+
+    }
+
+    let toggle2 = true;
+    function FilteriPads() {
+        let elementCount = document.getElementsByClassName('category2');
+        if (toggle2) {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category1')[i].style.visibility = 'hidden';
+                document.getElementsByClassName('category3')[i].style.visibility = 'hidden';
+
+            }
+        }
+        else {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category1')[i].style.visibility = 'visible';
+                document.getElementsByClassName('category3')[i].style.visibility = 'visible';
+            }
+        }
+
+        toggle2 = !toggle2
+    }
+
+
+    let toggle3 = true;
+    function FilterMacBooks() {
+        let elementCount = document.getElementsByClassName('category1');
+        if (toggle3) {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category1')[i].style.visibility = 'hidden';
+                document.getElementsByClassName('category2')[i].style.visibility = 'hidden';
+            }
+        }
+        else {
+            for (var i = 0; i < elementCount.length; i++) {
+                document.getElementsByClassName('category1')[i].style.visibility = 'visible';
+                document.getElementsByClassName('category2')[i].style.visibility = 'visible';
+            }
+        }
+
+        toggle3 = !toggle3
     }
 
     useEffect(() => {
@@ -36,10 +105,27 @@ export default function ProductList() {
         <>
             {/* {HandleProducts()} */}
             <div className="bg-white">
-                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+                <div className="container mx-auto py-16 px-4 lg:max-w-7xl lg:px-8 flex">
+                    {/* <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> */}
+                    {/* <div className="container mx-auto py-16 px-4"> */}
                     {/* <h2 className="">Products</h2> */}
                     <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                         {DisplayProductList()}
+                    </div>
+                    <div className="flex flex-col p-10 px-10">
+                        <div className="p-4">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-10 rounded" onClick={() => clearFilters()}>Clear Filters</button>
+                        </div>
+                        <div className="p-4">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-10 rounded" onClick={() => FilteriPhones()}>Filter iPhones</button>
+                        </div>
+                        <div className="p-4">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-10 rounded" onClick={() => FilteriPads()}>Filter iPads</button>
+                        </div>
+                        <div className="p-4">
+                            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-10 rounded" onClick={() => FilterMacBooks()}>Filter MacBooks</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- hartley_react/src/components/single-product.js
+++ hartley_react/src/components/single-product.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Reviews from "./etc/DisplayReview";
+
 
 export default function SingleProd(props) {
 
   let { id } = useParams();
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState([]);
 
   function HandleSingleProduct() {
     axios.get("http://localhost:80/singleprod.php?id=" + id)
       .then(response => {
-        console.log(response);
         setProduct(product => (response.data));
-        console.log(product);
       })
       .catch(function (error) {
         console.log("Error getting products", error)
@@ -22,61 +22,39 @@ export default function SingleProd(props) {
 
   useEffect(() => {
     HandleSingleProduct();
-  }, []);
+  }, [product.id]);
+  //checking if the id of that object is different, if it is re render. 
 
+  function DisplayReviews() {
+    for (let i = 0; i < product.review; i++) {
+      return <Reviews />
+    }
+
+  }
 
   return (
     <div className="bg-white">
       <div className="pt-6">
         <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
           <div className="hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block mx-auto">
-            <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg" alt="Two each of gray, white, and black shirts laying flat." className="w-full h-full object-center object-cover"></img>
+            <img src={"../images/" + product.image + ".jpg"} alt="Two each of gray, white, and black shirts laying flat." className="w-full h-full object-center object-cover"></img>
           </div>
-          {/* <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
-            <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="Model wearing plain black basic tee." className="w-full h-full object-center object-cover"></img>
-            </div>
-            <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="Model wearing plain gray basic tee." className="w-full h-full object-center object-cover"></img>
-            </div>
-          </div>
-          <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
-            <img src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="Model wearing plain white basic tee." className="w-full h-full object-center object-cover"></img>
-          </div> */}
         </div>
 
         <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">Basic Tee 6-Pack</h1>
+            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
           </div>
 
           <div className="mt-4 lg:mt-0 lg:row-span-3">
             <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl text-gray-900">$192</p>
+            <p className="text-3xl text-gray-900">${product.price}</p>
 
             <div className="mt-6">
               <h3 className="sr-only">Reviews</h3>
               <div className="flex items-center">
                 <div className="flex items-center">
-                  <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-
-                  <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-
-                  <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-
-                  <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-
-                  <svg className="text-gray-200 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
+                  {DisplayReviews()}
                 </div>
                 <p className="sr-only">4 out of 5 stars</p>
                 <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
@@ -95,33 +73,13 @@ export default function SingleProd(props) {
               <h3 className="sr-only">Description</h3>
 
               <div className="space-y-6">
-                <p className="text-base text-gray-900">The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: &quot;Black&quot;. Need to add an extra pop of color to your outfit? Our white tee has you covered.</p>
+                <p className="text-base text-gray-900">{product.des}</p>
               </div>
             </div>
 
-            <div className="mt-10">
-              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
 
-              <div className="mt-4">
-                <ul role="list" className="pl-4 list-disc text-sm space-y-2">
-                  <li className="text-gray-400"><span className="text-gray-600">Hand cut and sewn locally</span></li>
 
-                  <li className="text-gray-400"><span className="text-gray-600">Dyed with our proprietary colors</span></li>
 
-                  <li className="text-gray-400"><span className="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
-
-                  <li className="text-gray-400"><span className="text-gray-600">Ultra-soft 100% cotton</span></li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="mt-10">
-              <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-              <div className="mt-4 space-y-6">
-                <p className="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
-              </div>
-            </div>
           </div>
         </div>
       </div>
